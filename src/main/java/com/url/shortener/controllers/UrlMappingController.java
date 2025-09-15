7b6a82176fe4adb75a2ac651e05a46303c5f6f01
Dnyanesh Agale <dnyanesh0019@gmail.com>
2025-09-15 22:45:28 +0530

    URL Mapping Done

---
+++ src/main/java/com/url/shortener/controllers/UrlMappingController.java
@@ -0,0 +1,42 @@
+package com.url.shortener.controllers;
+
+import com.url.shortener.dtos.UrlMappingDTO;
+import com.url.shortener.models.User;
+import com.url.shortener.service.UrlMappingService;
+import com.url.shortener.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/urls")
+@AllArgsConstructor
+public class UrlMappingController {
+    private UrlMappingService urlMappingService;
+    private UserService urlService;
+
+    // {"originalUrl" : "https://example.com"}
+  // https://abc.com/RLUSAPNi --> https://example.com
+    @PostMapping("/shorten")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<UrlMappingDTO> createShortUrl(@RequestBody Map<String, String> request,
+                                                        Principal principal) {
+        String originalUrl = request.get("originalUrl");
+        User user = urlService.findByUsername(principal.getName());
+        UrlMappingDTO urlMappingDTO = urlMappingService.createShortUrl(originalUrl, user);
+        return ResponseEntity.ok(urlMappingDTO);
+    }
+
+    @GetMapping("/myurls")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<List<UrlMappingDTO>> createShortUrl (Principal principal) {
+        User user = urlService.findByUsername(principal.getName());
+        List<UrlMappingDTO> urls = urlMappingService.getUrlsByUser(user);
+        return ResponseEntity.ok(urls);
+    }
+}
